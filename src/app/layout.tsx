9b0f27a73eed2272c6d236290dc7bc385b7eba2d
Md. Tarek Hasan <tarekhasan.tkg101@gmail.com>
2024-10-28 15:05:25 +0600

    meta data added for google verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Digital Cowboy",
-  description: "Digital Solutions with a Maverick’s Edge.",
+  description: "Your partner in the digital frontier.",
 };
 
 export default function RootLayout({
@@ -18,6 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta name="google-site-verification" content="j1p4ekYU6LmzxOzBBmnjRZADcx8CHpwo-RYsznz2N14" />
       <body className={inter.className}>
         <Navbar/>
         {children}
